95b8239a75feb5dd0b21107be20a6666557f08f3
anjalidoshi13 <anjalikothari05222@gmail.com>
2023-05-10 14:16:18 +0530

    animal done

--- week-1/day-2/exercise-3/AnimalExercise/Program.cs
+++ week-1/day-2/exercise-3/AnimalExercise/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace AnimalExercise
+﻿abstract class Animal
 {
-    internal class Program
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public abstract void MakeSound();
+}
+class Dog : Animal, IMovable
+{
+    public override void MakeSound()
     {
-        static void Main(string[] args)
-        {
-            // Create a list of Animal objects, add Dog and Cat instances, and call their methods
-        }
+        Console.WriteLine("Woof");
+    }
+    public void Move()
+    {
+        Console.WriteLine("Dog is running");
     }
 }
+class Cat : Animal, IMovable
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+    public void Move()
+    {
+        Console.WriteLine("Cat is walking");
+    }
+}
+interface IMovable
+{
+    void Move();
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Animal> animals = new List<Animal>();
+        Dog dog = new Dog { Name = "kutta", Age = 3 };
+        Cat cat = new Cat { Name = "billi", Age = 2 };
+        animals.Add(dog);
+        animals.Add(cat);
+        foreach (Animal animal in animals)
+        {
+            animal.MakeSound();
+            if (animal is IMovable movable)
+            {
+                movable.Move();
+            }
+        }
+    }
