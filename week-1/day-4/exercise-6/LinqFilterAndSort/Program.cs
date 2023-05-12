ae35316e8ddca5db0cc86b130013e99c3b990ff1
anjalidoshi13 <anjalikothari05222@gmail.com>
2023-05-12 16:40:54 +0530

    disponse

--- week-1/day-4/exercise-6/LinqFilterAndSort/Program.cs
+++ week-1/day-4/exercise-6/LinqFilterAndSort/Program.cs
@@ -1,12 +1,31 @@
-﻿namespace LinqFilterAndSort
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class Person
 {
-    internal class Program
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+}
+class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        // Create a list of Person objects with different properties
+        List<Person> people = new List<Person>
         {
-            // Create a list of Person objects
-            // Use LINQ to filter and sort the list
-            // Print the filtered and sorted list of people to the console
+            new Person { FirstName = "ANJALI", LastName = "KOTHARI", Age = 22 },
+            new Person { FirstName = "PRIYANKA", LastName = "PARMAR", Age = 23 },
+            new Person { FirstName = "NIKEETA", LastName = "PATEL", Age = 30 },
+             new Person { FirstName = "URVASHI", LastName = "PARMAR", Age = 16 }
+        };
+        var filteredPeople = people.Where(p => p.Age >= 18)
+                                   .OrderBy(p => p.LastName)
+                                   .ThenBy(p => p.FirstName);
+        // Print the filtered and sorted list of people to the console
+        foreach (var person in filteredPeople)
+        {
+            Console.WriteLine($"Name: {person.FirstName} {person.LastName}, Age: {person.Age}");
         }
     }
 }
--- week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
+++ week-1/day-5/exercise-1/FileLoggerDisposableApp/Program.cs
@@ -4,27 +4,42 @@
     {
         static void Main(string[] args)
         {
-            // Use FileLogger and dispose of it properly
+            using (var logger = new FileLogger("log.txt"))
+            {
+                logger.Log("This is a log message.");
+                logger.Log("This is another log message.");
+            }
         }
     }
-
     class FileLogger : IDisposable
     {
         private StreamWriter _writer;
-
         public FileLogger(string filePath)
         {
-            // Initialize StreamWriter instance
+            _writer = new StreamWriter(filePath);
         }
-
         public void Dispose()
         {
-            // Implement IDisposable pattern
+            Dispose(true);
+            GC.SuppressFinalize(this);
         }
-
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (_writer != null)
+                {
+                    _writer.Dispose();
+                    _writer = null;
+                }
+            }
+        }
         public void Log(string message)
         {
-            // Write message to the file
+            {
+                _writer.WriteLine($"{DateTime.UtcNow}: {message}");
+                _writer.Flush();
+            }
         }
     }
 }
